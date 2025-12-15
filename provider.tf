fa938fe494a028a9ed487705ad62fe809ec8750e
willzhao <595572829@qq.com>
2025-12-15 10:43:03 +0000

    update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     alicloud = {
-      source   = "aliyun/alicloud"
+      source  = "aliyun/alicloud"
+      version = "1.227.1"
     }
   }
 }
--- variables.tf
+++
@@ -1,3 +0,0 @@
-variable "region" {
-  default = "cn-chengdu"
-}
