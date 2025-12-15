0b2de35fb2c7e7e7ebae5555f30256137762ff57
willzhao <595572829@qq.com>
2025-12-15 06:58:13 +0000

    update para

--- main.tf
+++ main.tf
@@ -1,45 +1,46 @@
 provider "alicloud" {
-    region = "cn-hangzhou" # Replace with your desired region
+  region = "cn-hangzhou"
 }
 
 resource "alicloud_vpc" "main" {
-    name       = "my-vpc"
-    cidr_block = "192.168.0.0/16"
+  vpc_name   = "my-vpc"
+  cidr_block = "192.168.0.0/16"
 }
 
 resource "alicloud_vswitch" "main" {
-    vpc_id            = alicloud_vpc.main.id
-    cidr_block        = "192.168.1.0/24"
-    availability_zone = "cn-hangzhou-b" # Replace with your desired zone
-    name              = "my-vswitch"
+  vpc_id       = alicloud_vpc.main.id
+  cidr_block   = "192.168.1.0/24"
+  zone_id      = "cn-hangzhou-b"   # 已改用 zone_id
+  vswitch_name = "my-vswitch"
 }
 
 resource "alicloud_security_group" "main" {
-    name        = "my-security-group"
-    description = "Security group for ECS instance"
+  security_group_name = "my-security-group"
+  vpc_id              = alicloud_vpc.main.id
+  description         = "Security group for ECS instance"
 }
 
 resource "alicloud_security_group_rule" "allow_ssh" {
-    type              = "ingress"
-    ip_protocol       = "tcp"
-    port_range        = "22/22"
-    priority          = 1
-    security_group_id = alicloud_security_group.main.id
-    cidr_ip           = "0.0.0.0/0"
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  port_range        = "22/22"
+  priority          = 1
+  security_group_id = alicloud_security_group.main.id
+  cidr_ip           = "0.0.0.0/0"
 }
 
 resource "alicloud_instance" "main" {
-    instance_name       = "my-ecs-instance"
-    availability_zone   = alicloud_vswitch.main.availability_zone
-    instance_type       = "ecs.t5-lc2m1.nano" # Replace with your desired instance type
-    security_group_id   = alicloud_security_group.main.id
-    vswitch_id          = alicloud_vswitch.main.id
-    image_id            = "ubuntu_20_04_x64_20G_alibase_20230618.vhd" # Replace with your desired image
-    internet_max_bandwidth_out = 10
-    allocate_public_ip  = true
-    password            = "YourPassword123!" # Replace with a secure password
+  instance_name      = "my-ecs-instance"
+  instance_type      = "ecs.t5-lc2m1.nano"      # 可根据需求修改
+  vswitch_id         = alicloud_vswitch.main.id
+  security_group_ids = [alicloud_security_group.main.id]  # 集合而不是单个 id
+  zone_id            = alicloud_vswitch.main.zone_id      # 推荐和 vswitch 保持一致
+  image_id           = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
+  internet_max_bandwidth_out = 10
+  allocate_public_ip = true
+  password           = "YourPassword123!"                 # 强烈建议使用变量或环境变量
 }
 
 output "instance_public_ip" {
-    value = alicloud_instance.main.public_ip
+  value = alicloud_instance.main.public_ip
 }
