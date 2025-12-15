602d28249c21a00990335517bb6604126481f77f
willzhao <595572829@qq.com>
2025-12-15 07:26:36 +0000

    only create vpc

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     alicloud = {
       source  = "aliyun/alicloud"
-      version = "~> 1.240.0"  # 使用最新稳定版
+      version = "~> 1.230. 0"
     }
   }
 }
@@ -12,43 +11,26 @@ provider "alicloud" {
   region = "cn-hangzhou"
 }
 
+# 创建 VPC
 resource "alicloud_vpc" "main" {
-  vpc_name   = "my-vpc"       # 新版用 vpc_name
-  cidr_block = "192.168.0.0/16"
+  vpc_name    = "my-vpc"
+  cidr_block  = "192.168.0.0/16"
+  description = "My VPC for testing"
 }
 
+# 创建 VSwitch（交换机）
 resource "alicloud_vswitch" "main" {
   vpc_id       = alicloud_vpc.main. id
   cidr_block   = "192.168.1.0/24"
-  zone_id      = "cn-hangzhou-b"  # 新版用 zone_id
-  vswitch_name = "my-vswitch"      # 新版用 vswitch_name
+  zone_id      = "cn-hangzhou-b"
+  vswitch_name = "my-vswitch"
 }
 
-resource "alicloud_security_group" "main" {
-  security_group_name = "my-security-group"  # 新版用 security_group_name
-  vpc_id              = alicloud_vpc.main.id
-  description         = "Security group for ECS instance"
+# 输出 VPC ID
+output "vpc_id" {
+  value = alicloud_vpc.main.id
 }
 
-resource "alicloud_security_group_rule" "allow_ssh" {
-  type              = "ingress"
-  ip_protocol       = "tcp"
-  port_range        = "22/22"
-  priority          = 1
-  security_group_id = alicloud_security_group.main.id
-  cidr_ip           = "0.0.0.0/0"
-}
-
-resource "alicloud_instance" "main" {
-  instance_name              = "my-ecs-instance"
-  instance_type              = "ecs.t5-lc2m1.nano"
-  vswitch_id                 = alicloud_vswitch.main. id
-  security_groups            = [alicloud_security_group. main.id]
-  image_id                   = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
-  internet_max_bandwidth_out = 10
-  password                   = "YourPassword123!"
-}
-
-output "instance_public_ip" {
-  value = alicloud_instance.main.public_ip
+output "vswitch_id" {
+  value = alicloud_vswitch.main.id
 }
