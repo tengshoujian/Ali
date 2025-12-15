d56149d9d62a0d4cd837c1f4cae6df67d2174cdf
willzhao <595572829@qq.com>
2025-12-15 07:00:38 +0000

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "alicloud_vpc" "main" {
 resource "alicloud_vswitch" "main" {
   vpc_id       = alicloud_vpc.main.id
   cidr_block   = "192.168.1.0/24"
-  zone_id      = "cn-hangzhou-b"   # 已改用 zone_id
+  zone_id      = "cn-hangzhou-b"
   vswitch_name = "my-vswitch"
 }
 
@@ -30,17 +30,15 @@ resource "alicloud_security_group_rule" "allow_ssh" {
 }
 
 resource "alicloud_instance" "main" {
-  instance_name      = "my-ecs-instance"
-  instance_type      = "ecs.t5-lc2m1.nano"      # 可根据需求修改
-  vswitch_id         = alicloud_vswitch.main.id
-  security_group_ids = [alicloud_security_group.main.id]  # 集合而不是单个 id
-  zone_id            = alicloud_vswitch.main.zone_id      # 推荐和 vswitch 保持一致
-  image_id           = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
+  instance_name              = "my-ecs-instance"
+  instance_type              = "ecs.t5-lc2m1.nano"
+  vswitch_id                 = alicloud_vswitch.main.id
+  security_groups            = [alicloud_security_group.main.id]  # 用 security_groups 而非 security_group_ids
+  image_id                   = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
   internet_max_bandwidth_out = 10
-  allocate_public_ip = true
-  password           = "YourPassword123!"                 # 强烈建议使用变量或环境变量
+  password                   = "YourPassword123!"
 }
 
 output "instance_public_ip" {
-  value = alicloud_instance.main.public_ip
+  value = alicloud_instance.main. public_ip
 }
