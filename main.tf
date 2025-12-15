a0dcc20fb7cc50c31b413608cd5e8ea92923f064
willzhao <595572829@qq.com>
2025-12-15 11:07:46 +0000

    connect to Ali

--- main.tf
+++ main.tf
@@ -1,66 +1,62 @@
 provider "alicloud" {
-  region = "cn-hangzhou"
+  region = var.region
 }
 
-# 创建 VPC
-resource "alicloud_vpc" "main" {
-  name       = "my-vpc"
-  cidr_block = "192.168.0.0/16"
+
+data "alicloud_zones" "default" {
+  available_disk_category     = "cloud_essd"
+  available_resource_creation = "VSwitch"
+  available_instance_type     = var.instance_type
 }
 
-# 创建 VSwitch
-resource "alicloud_vswitch" "main" {
-  vpc_id            = alicloud_vpc.main.id
-  cidr_block        = "192.168.1.0/24"
-  availability_zone = "cn-hangzhou-b"
-  name              = "my-vswitch"
+resource "alicloud_vpc" "vpc" {
+  vpc_name   = var.instance_name
+  cidr_block = "172.16.0.0/12"
 }
 
-# 创建安全组
-resource "alicloud_security_group" "main" {
-  name   = "my-security-group"
-  vpc_id = alicloud_vpc.main.id
+resource "alicloud_vswitch" "vsw" {
+  vpc_id     = alicloud_vpc.vpc.id
+  cidr_block = "172.16.0.0/21"
+  zone_id    = data.alicloud_zones.default.zones.0.id
 }
 
-# 添加 SSH 规则
-resource "alicloud_security_group_rule" "allow_ssh" {
+resource "alicloud_security_group" "default" {
+  name   = var.instance_name
+  vpc_id = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_security_group_rule" "allow_tcp_22" {
   type              = "ingress"
   ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
   port_range        = "22/22"
   priority          = 1
-  security_group_id = alicloud_security_group. main.id
+  security_group_id = alicloud_security_group.default.id
   cidr_ip           = "0.0.0.0/0"
 }
 
-# 创建 ECS 实例
-resource "alicloud_instance" "default" {
-  # 必需参数
-  instance_type  = "ecs. t5-lc2m1.nano"
-  image_id       = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
-  security_groups = [alicloud_security_group.main.id]
-  vswitch_id     = alicloud_vswitch.main.id
-  
-  # 可选参数
-  instance_name              = "my-instance"
-  internet_max_bandwidth_out = 10
-  allocate_public_ip         = true
-  password                   = "YourPassword123!"
-  
-  # 你的可选参数
-  enable_jumbo_frame    = false
-  deletion_protection   = false
-  system_disk_encrypted = false
-  force_delete          = false
-  dry_run               = false
-  maintenance_notify    = false
-  include_data_disks    = true
-  is_outdated           = false
-  period_unit           = "Month"
-  renewal_status        = "Normal"
-  auto_renew_period     = 1
+
+
+
+
+
+
+
+
+resource "alicloud_instance" "instance" {
+  count                      = var.ecs_count
+  availability_zone          = data.alicloud_zones.default.zones.0.id
+  security_groups            = alicloud_security_group.default.*.id
+  password                   = var.password
+  instance_type              = var.instance_type
+  system_disk_category       = "cloud_essd"
+  image_id                   = var.image_id
+  instance_name              = var.instance_name
+  vswitch_id                 = alicloud_vswitch.vsw.id
+  internet_max_bandwidth_out = var.internet_bandwidth
 }
 
-# 输出公网 IP
-output "instance_public_ip" {
-  value = alicloud_instance.default. public_ip
+output "public_ip" {
+  value = [for i in range(var.ecs_count) : alicloud_instance.instance[i].public_ip]
 }
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "instance_type" {
+  type    = string
+  default = "ecs.e-c1m2.large"
+}
+
+variable "image_id" {
+  type    = string
+  default = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+}
+
+variable "region" {
+  type    = string
+  default = "cn-huhehaote"
+}
+
+variable "internet_bandwidth" {
+  type    = string
+  default = "10"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "tf-sample"
+}
+
+variable "password" {
+  type    = string
+  default = "Test@12345"
+}
+
+variable "ecs_count" {
+  type    = number
+  default = 1
+}
