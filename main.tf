6201111de74f2fd1664aa5c60bb0d01b7b93ea8c
willzhao <595572829@qq.com>
2025-12-15 10:31:17 +0000

    follow up document

--- main.tf
+++ main.tf
@@ -1,36 +1,81 @@
-terraform {
-  required_providers {
-    alicloud = {
-      source  = "aliyun/alicloud"
-      version = "~> 1.230.0"
-    }
-  }
-}
+# main.tf
 
 provider "alicloud" {
-  region = "cn-hangzhou"
+  region = var.region
 }
 
-# 创建 VPC
-resource "alicloud_vpc" "main" {
-  vpc_name    = "my-vpc"
-  cidr_block  = "192.168.0.0/16"
-  description = "My VPC for testing"
+variable "instance_name" {
+  default = "tf-sample"
 }
 
-# 创建 VSwitch（交换机）
-resource "alicloud_vswitch" "main" {
-  vpc_id       = alicloud_vpc.main. id
-  cidr_block   = "192.168.1.0/24"
-  zone_id      = "cn-hangzhou-b"
-  vswitch_name = "my-vswitch"
+variable "instance_type" {
+  default = "ecs.e-c1m2.large"
 }
 
-# 输出 VPC ID
-output "vpc_id" {
-  value = alicloud_vpc.main.id
+data "alicloud_zones" "default" {
+  available_disk_category     = "cloud_essd"
+  available_resource_creation = "VSwitch"
+  available_instance_type     = var.instance_type
 }
 
-output "vswitch_id" {
-  value = alicloud_vswitch.main.id
+resource "alicloud_vpc" "vpc" {
+  vpc_name   = var.instance_name
+  cidr_block = "172.16.0.0/12"
 }
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id     = alicloud_vpc.vpc.id
+  cidr_block = "172.16.0.0/21"
+  zone_id    = data.alicloud_zones.default.zones.0.id
+}
+
+resource "alicloud_security_group" "default" {
+  name   = var.instance_name
+  vpc_id = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_security_group_rule" "allow_tcp_22" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "22/22"
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
+  cidr_ip           = "0.0.0.0/0"
+}
+
+variable "image_id" {
+  default = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+}
+
+variable "internet_bandwidth" {
+  default = "10"
+}
+
+variable "password" {
+  default = "Test@12345"
+}
+
+variable "ecs_count" {
+  default = 1
+}
+
+resource "alicloud_instance" "instance" {
+  count                      = var.ecs_count
+  availability_zone          = data.alicloud_zones.default.zones.0.id
+  security_groups            = alicloud_security_group.default.*.id
+  password                   = var.password
+  instance_type              = var.instance_type
+  system_disk_category       = "cloud_essd"
+  image_id                   = var.image_id
+  instance_name              = var.instance_name
+  vswitch_id                 = alicloud_vswitch.vsw.id
+  internet_max_bandwidth_out = var.internet_bandwidth
+}
+
+output "public_ip" {
+  value = [for i in range(var.ecs_count) : alicloud_instance.instance[i].public_ip]
+}
+
+
