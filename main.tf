063642d751ca6d75ed363c462cfb73dfb78e7bf4
willzhao <595572829@qq.com>
2025-12-15 07:06:54 +0000

    old provider

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     alicloud = {
       source  = "aliyun/alicloud"
-      version = "~> 1.230.0"
+      version = "~> 1.118.0"  # 使用稳定的旧版本，避免参数不兼容
     }
   }
 }
 
 provider "alicloud" {
   region = "cn-hangzhou"
-  # 不需要显式配置 access_key 和 secret_key
-  # Terraform 会自动读取环境变量 ALICLOUD_ACCESS_KEY 和 ALICLOUD_SECRET_KEY
+  # 环境变量 ALICLOUD_ACCESS_KEY 和 ALICLOUD_SECRET_KEY 会自动读取
 }
 
 resource "alicloud_vpc" "main" {
-  vpc_name   = "my-vpc"
+  name       = "my-vpc"
   cidr_block = "192.168.0.0/16"
 }
 
 resource "alicloud_vswitch" "main" {
-  vpc_id       = alicloud_vpc.main.id
-  cidr_block   = "192.168.1.0/24"
-  zone_id      = "cn-hangzhou-b"
-  vswitch_name = "my-vswitch"
+  vpc_id            = alicloud_vpc.main.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "cn-hangzhou-b"
+  name              = "my-vswitch"
 }
 
 resource "alicloud_security_group" "main" {
-  security_group_name = "my-security-group"
-  vpc_id              = alicloud_vpc. main.id
-  description         = "Security group for ECS instance"
+  name        = "my-security-group"
+  vpc_id      = alicloud_vpc.main.id
+  description = "Security group for ECS instance"
 }
 
 resource "alicloud_security_group_rule" "allow_ssh" {
@@ -43,7 +43,7 @@ resource "alicloud_security_group_rule" "allow_ssh" {
 resource "alicloud_instance" "main" {
   instance_name              = "my-ecs-instance"
   instance_type              = "ecs.t5-lc2m1.nano"
-  vswitch_id                 = alicloud_vswitch. main.id
+  vswitch_id                 = alicloud_vswitch.main.id
   security_groups            = [alicloud_security_group.main.id]
   image_id                   = "ubuntu_20_04_x64_20G_alibase_20230618.vhd"
   internet_max_bandwidth_out = 10
@@ -51,5 +51,5 @@ resource "alicloud_instance" "main" {
 }
 
 output "instance_public_ip" {
-  value = alicloud_instance.main.public_ip
+  value = alicloud_instance.main. public_ip
 }
