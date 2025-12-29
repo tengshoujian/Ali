1a445abc3c2adeba94451d738c1ee5ed004b07bc
willzhao <595572829@qq.com>
2025-12-29 07:41:24 +0000

    update

--- output.tf
+++ output.tf
@@ -15,7 +15,7 @@ output "private_ip" {
 
 output "ssh_command" {
   description = "SSH 连接命令（新用户）"
-  value       = "ssh ${var.username}@${alicloud_instance.server.public_ip}"
+  value       = "ssh ${var.username}@${alicloud_instance.spot_instance.public_ip}"
 }
 
 output "docker_info" {
