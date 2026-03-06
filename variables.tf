65fcd920b9596884547f6cd7a58504fbe9832624
willzhao <595572829@qq.com>
2026-03-06 20:11:40 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "instance_name" {
 variable "instance_type" {
   description = "ECS 实例规格"
   type        = string
-  default     = "ecs.e-c1m4.large"
+  default     = "ecs.e-c1m2.xlarge"
 }
 
 variable "image_id" {
